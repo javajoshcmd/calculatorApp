b4f80284427d7a0e4825cbed5e17ed8c7ef76460
Josh Howcroft <Josh@Joshs-iMac.local>
2021-05-07 13:16:10 +0100

    file sys clean

---
+++ src/App.js
@@ -0,0 +1,60 @@
+import React from "react";
+import { Button } from "./components/Button";
+import "./App.css";
+import { Input } from "./components/Input";
+import { ClearButton } from "./components/ClearButton";
+import * as math from "mathjs";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { input: "" };
+  }
+
+  addToInput = (val) => {
+    this.setState({ input: this.state.input + val });
+  };
+
+  handleEqual = () => {
+    this.setState({ input: math.evaluate(this.state.input) });
+  };
+  render() {
+    return (
+      <div className="App">
+        <h1 className="header">React Calculator</h1>
+        <div className="calc-wrapper">
+          <Input input={this.state.input}></Input>
+          <div className="row">
+            <Button handleClick={this.addToInput}>7</Button>
+            <Button handleClick={this.addToInput}>8</Button>
+            <Button handleClick={this.addToInput}>9</Button>
+            <Button handleClick={this.addToInput}>/</Button>
+          </div>
+          <div className="row">
+            <Button handleClick={this.addToInput}>4</Button>
+            <Button handleClick={this.addToInput}>5</Button>
+            <Button handleClick={this.addToInput}>6</Button>
+            <Button handleClick={this.addToInput}>X</Button>
+          </div>
+          <div className="row">
+            <Button handleClick={this.addToInput}>1</Button>
+            <Button handleClick={this.addToInput}>2</Button>
+            <Button handleClick={this.addToInput}>3</Button>
+            <Button handleClick={this.addToInput}>+</Button>
+          </div>
+          <div className="row">
+            <Button handleClick={this.addToInput}>.</Button>
+            <Button handleClick={this.addToInput}>0</Button>
+            <Button handleClick={() => this.handleEqual()}>=</Button>
+            <Button handleClick={this.addToInput}>-</Button>
+          </div>
+          <ClearButton handleClear={() => this.setState({ input: "" })}>
+            Clear
+          </ClearButton>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
